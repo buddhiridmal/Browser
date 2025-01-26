8794749282f14e4b249d12a530810ef0455a1dfe
Buddhi Ridmal <buddhiridmal@gmail.com>
2025-01-26 14:10:27 +0530

    Add socket connection, with input stream handling using a separate thread

--- src/main/java/browser/ClientApp.java
+++ src/main/java/browser/ClientApp.java
@@ -1,4 +1,23 @@
 package browser;
 
-public class ClientApp {
-}
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ClientApp  {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("google.lk", 80);
+        System.out.println("connected to google with the socket "+ socket.getRemoteSocketAddress());
+
+        new Thread(()->{
+            try {
+                InputStream is = socket.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }).start();
+
+    }}
